590b89e940932ded6d4c52dac0df1b55c94de7a0
Anthony Lowhur <vanstorm9@users.noreply.github.com>
2015-09-13 01:58:16 -0400

    Create Motor.cs

---
+++ Ardunio Test/Assets/Motor.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class Motor : MonoBehaviour {
+	public SerialPort serial = new SerialPort ("COM7",9600);
+	//	Debug.Log("Initalized serial port");
+	private bool lightState = false;
+	//public GameObject light = null;
+	public AudioClip clip;
+	
+	int wheel_time = 0;
+	
+	
+	void Start()
+	{
+		serial.Open ();
+		//serial.Write ("Testing");
+
+		OpenCheck ();
+	}
+	
+	void OpenCheck()
+	{
+		if (serial.IsOpen == false) {
+			//serial.Write ("Force Open");
+			Debug.Log ("Force Open");
+			serial.Open ();
+		} else {
+			//serial.Write ("Port already opened");
+			Debug.Log ("Port already opened");
+		}
+	}
+	
+	/*
+	void Update()
+	{
+		if (lightState) 
+		{
+			serial.Write ("A");
+		}
+	}
+	*/
+	void OnCollisionEnter(Collision collision) 
+	{
+		FingerModel finger = collision.gameObject.GetComponentInParent<FingerModel>();
+		/*
+		if(finger){
+			Debug.Log ("Finger " + finger.fingerType);
+
+		}
+		*/
+
+
+		OpenCheck ();
+		Debug.Log ("Serial sent to Arduino");
+		serial.Write ("A");
+
+
+
+	}
+	
+	public void OnMouseDown(){
+		Debug.Log ("Mouse pressed Down");
+		OpenCheck ();
+		/*
+		while (true) 
+		{
+
+			if(wheel_time < 30)
+			{
+				Debug.Log ("Escaped");
+				break;
+			}
+			serial.Write("A");
+			wheel_time++;
+			Debug.Log (wheel_time);
+		}
+		*/
+		
+		
+		serial.Write ("A");
+		
+	}
+}
